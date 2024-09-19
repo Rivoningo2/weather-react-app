5c26408bdc22c69a7376afd493efb8d7d795f21e
Rivoningo Vision Hlungwane <hlungwanevision@gmail.com>
2024-09-19 08:17:57 +0200

    Installed axios
    I installed axios to my code and tried to make my data real life using API

--- src/WeatherTemplate.js
+++ src/WeatherTemplate.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import './WeatherTemplate.css'
-export default function WeatherTemplate ()
-{
+import axios from "axios";
+export default function WeatherTemplate (props)
+{ 
+  const [weatherData, setweatherData] = useState({ready: false});
+ 
+  let city = "Centurion";
+
+
+  function handleResponse(response) {
+  console.log(response);
+  setweatherData({
+    ready: true,
+    temperature: Math.round(response.data.main.temp),
+    wind: response.data.wind.speed,
+    name: response.data.name,
+    icon: response.data.weather[0].icon,
+    humidty: response.data.main.humidity,
+    feels_like: Math.round(response.data.main.feels_like),
+    description: response.data.weather[0].description,
+
+  })
+ 
+ 
+}
+
+    if (weatherData.ready) {
+      
+    
     return (
        <div className = "WeatherTemplate container">
          <div className="form ">
          <form>
-         <div className="row">
+         <div className="row mb-5">
          <div className="col-9">
-         <input type="email" className="form-control"  aria-describedby="emailHelp"> 
+         <input type="email" className="form-control w-100"  aria-describedby="emailHelp" placeholder="Enter a city" AutoFocus="on"> 
          </input>
          </div>
   
          <div className="col-3">
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-primary ">Submit</button>
          </div>
          </div>
          </form>
@@ -22,35 +48,45 @@ export default function WeatherTemplate ()
          <div className="data">
         <div className="row">
         <div className="col-6">
-        <img href="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png" alt="weather-icon"/>
-        11º c
+        <img href={weatherData.icon}/>
+        <span className="currentTemp">{weatherData.temperature}</span> <span className="units">ºc</span>
         </div>
         <div className="col-6">
-        Humidity: 73%
+        Humidity: {weatherData.humidty}%
         <br/>
-        Wind: 1 km/h
+        Wind: {weatherData.wind} km/h
         </div>
         </div>
         </div>
      <div className="current">
        <h1>
-        Johannesburg
+        {weatherData.name}
        </h1>
        <ul>
         <li>
-        Feels like 10°
+        Feels like {weatherData.feels_like}°
         </li>
         <li>
-        Monday, 05:01
+       Thurday, 19:20
         </li>
-        <li>
-        clear sky
+        <li className="text-capitalize">
+        {weatherData.description}
         </li>
        </ul>
      </div>
+
     <footer>
       <a href="https://github.com/Rivoningo2/weather-react-app" target="_blank"  rel="noreferrer"> open-soure code</a> by Rivoningo Hlungwane
     </footer>
         </div>
       )
+
+}
+else{
+  const apiKey= "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return ("loading")
+}
 }
